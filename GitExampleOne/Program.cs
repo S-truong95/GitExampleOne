c61290c46c01a612895ef96c3cb209d417677ec3
Steven <steventruong1995@yahoo.com>
2019-09-04 11:40:35 -0400

    First Commit

---
+++ GitExampleOne/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GitExampleOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+            
+        {
+
+            string firstName = "Steven";
+            string lastName = "Truong";
+
+            int age = 24;
+
+            int puttingOnSomeYears = 40;
+
+            int oldAge = age + puttingOnSomeYears;
+
+            Console.WriteLine(firstName + " " + lastName);
+
+            Console.WriteLine("I am " + age + " years old");
+
+            Console.WriteLine("I'm really glad i'm not " + oldAge + " years old");
+
+
+        }
+    }
+}
